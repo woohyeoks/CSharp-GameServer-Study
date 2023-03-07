c3952d11dda7d6dcb4c1946dda33f42f21e687fa
woohyeok <woohyeoks807@naver.com>
2023-03-07 23:28:45 +0900

    기본 프로젝트 생성

---
+++ GameServerStudy/BasicMatchingServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicMatchingServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
